307594ed51f9be38370e81044f59f1d034d59934
Joan Renfroe <joan.renfroe@gmail.com>
2022-04-17 15:30:39 -0700

    got create rout working though not with the image

--- server.js
+++ server.js
@@ -1,67 +1,70 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const pokemon = require('./models/pokemon.js');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
+const pokemon = require("./models/pokemon.js");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
 const res = require("express/lib/response");
 const req = require("express/lib/request");
 
-
 //Middleware
-app.use(express.urlencoded({extended: false}));
-app.use(morgan('tiny'));
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: false }));
+// express.static(public)
+app.use(morgan("tiny"));
+app.use(methodOverride("_method"));
 
 
 
 // Index Route
-
-app.get('/pokemon/', (req, res) => {         
-    res.render('index.ejs', { pokemon: pokemon });                           
+app.get("/pokemon/", (req, res) => {
+  res.render("index.ejs", { pokemon: pokemon });
 });
 
 
 
-//New Route
-app.get('/pokemon/new', (req, res) => {
-    res.render('new.ejs')
- });
+//New Route - gives us the form to use for creating new pokemon
+app.get("/pokemon/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+
 
 // Show Route
-
-app.get('/pokemon/:id', (req, res) => {       
-    res.render('show.ejs', { onePokemon: pokemon[req.params.id] });
+app.get("/pokemon/:id", (req, res) => {
+  res.render("show.ejs", { onePokemon: pokemon[req.params.id] });
 });
+//create route is the post route for the show route
+app.post("/pokemon", (req, res) => {      
+  pokemon.push(req.body);
+  res.redirect("/pokemon");
+});
+
 
 
 
 //  Edit Route
 // app.get('/pokemon/:id/edit', (req, res) => {
 
-// });
+// });    
 
 
-// // Create Route
-app.post('/pokemon', (req, res) => {
-pokemon.push(req.body);
-res.redirect('/pokemon')
-});
 
 
-// // Update Route
+
+// Update Route
 // app.put('/pokemon/:id', (req, res) => {
 
 // });
 
 
 
+
 // // Delete Route
-// app.delete('pokemon/:id', (req, res) => {
-
+// app.delete("pokemon/:id", (req, res) => {
+//   pokemon.splice(req.params.id, 1);
+//   res.redirect("/pokemon");
 // });
-
 
 
 
@@ -69,5 +72,5 @@ res.redirect('/pokemon')
 
 
 app.listen(3000, () => {
-    console.log(`Express is listening on port ${PORT}`)
+  console.log(`Express is listening on port ${PORT}`);
 });
